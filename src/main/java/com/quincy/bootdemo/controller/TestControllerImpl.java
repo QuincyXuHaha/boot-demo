22b060b9726b561b22be3dd0e3fca991839d1331
quincy <895479640@qqc.om>
2019-07-09 17:19:27 +0800

    测试注解@RequestMapping在父子类上的表现

---
+++ src/main/java/com/quincy/bootdemo/controller/TestControllerImpl.java
@@ -0,0 +1,37 @@
+package com.quincy.bootdemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试
+ *
+ * @author xuguangquan
+ * @date 2019/7/9 星期二
+ */
+@Slf4j
+@RestController
+public class TestControllerImpl implements ITestController {
+
+    /**
+     * 测试结果：父类的方法的注解可以在子类上生效
+     * @return test
+     */
+    @Override
+    public String test() {
+        log.info("test");
+        return "test";
+    }
+
+    /**
+     * 测试结果：用的子类重写的方法上的注解而不是父类的
+     * @return hello
+     */
+    @GetMapping("hello2")
+    @Override
+    public String hello() {
+        log.info("hello,world.");
+        return "hello,world.";
+    }
+}
